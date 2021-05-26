9eac6441f2840a99dadb43fd6e0e889de4fcff36
Anton Kulyk <kuliks.anton@gmail.com>
2021-05-26 14:38:15 +0300

    Display PinnedItemsTable instead of PinnedItems

--- frontend/src/metabase/collections/containers/CollectionContent.jsx
+++ frontend/src/metabase/collections/containers/CollectionContent.jsx
@@ -10,7 +10,6 @@ import Search from "metabase/entities/search";
 import BulkActions from "metabase/collections/components/BulkActions";
 import Header from "metabase/collections/components/Header";
 import ItemList from "metabase/collections/components/ItemList";
-import PinnedItems from "metabase/collections/components/PinnedItems";
 import PinnedItemsTable from "metabase/collections/components/PinnedItemsTable";
 
 import ItemsDragLayer from "metabase/containers/dnd/ItemsDragLayer";
@@ -141,7 +140,7 @@ function CollectionContent({
                 collection={collection}
               />
 
-              <PinnedItems
+              <PinnedItemsTable
                 items={sortedPinnedItems}
                 collection={collection}
                 selected={selected}
@@ -151,8 +150,6 @@ function CollectionContent({
                 onMove={handleMove}
                 onCopy={handleCopy}
               />
-
-              <PinnedItemsTable items={sortedPinnedItems} />
 
               <Search.ListLoader query={unpinnedQuery} wrapped>
                 {({ list: unpinnedItems, metadata }) => {
