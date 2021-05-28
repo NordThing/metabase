c8c4485b1364f772b543af95ca82b2e9868f8872
alxnddr <alxnddr@gmail.com>
2021-05-29 00:55:58 +0300

    Audit Questions changes

---
+++ enterprise/frontend/src/metabase-enterprise/audit_app/containers/QuestionsAuditTable.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { t } from "ttag";
+
+import AuditTable from "./AuditTable";
+import AuditParameters from "../components/AuditParameters";
+
+import { table } from "../lib/cards/queries";
+
+const getSortOrder = isDescending => (isDescending ? "desc" : "asc");
+
+export function QuestionsAuditTable(props) {
+  const [sorting, setSorting] = useState({
+    column: "query_runs",
+    isDescending: true,
+  });
+
+  const handleSortingChange = sorting => setSorting(sorting);
+
+  return (
+    <AuditParameters
+      parameters={[
+        { key: "questionFilter", placeholder: t`Question name` },
+        { key: "collectionFilter", placeholder: t`Collection name` },
+      ]}
+    >
+      {({ questionFilter, collectionFilter }) => (
+        <AuditTable
+          {...props}
+          pageSize={50}
+          isSortable
+          sorting={sorting}
+          onSortingChange={handleSortingChange}
+          table={table(
+            questionFilter,
+            collectionFilter,
+            sorting.column,
+            getSortOrder(sorting.isDescending),
+          )}
+        />
+      )}
+    </AuditParameters>
+  );
+}
--- enterprise/frontend/src/metabase-enterprise/audit_app/lib/cards/question_detail.js
+++ enterprise/frontend/src/metabase-enterprise/audit_app/lib/cards/question_detail.js
@@ -1,11 +1,25 @@
 export const viewsByTime = (questionId: number) => ({
   card: {
-    name: "Views per day",
+    name: "Views last 45 days",
+    display: "bar",
+    dataset_query: {
+      type: "internal",
+      fn:
+        "metabase-enterprise.audit.pages.question-detail/cached-views-by-time",
+      args: [questionId, "day"],
+    },
+  },
+});
+
+export const averageExecutionTime = (questionId: number) => ({
+  card: {
+    name: "Average execution time last 45 days",
     display: "line",
     dataset_query: {
       type: "internal",
-      fn: "metabase-enterprise.audit.pages.question-detail/views-by-time",
-      args: [questionId, "day"], // FIXME: should this be automatic?
+      fn:
+        "metabase-enterprise.audit.pages.question-detail/avg-execution-time-by-time",
+      args: [questionId, "day"],
     },
   },
 });
