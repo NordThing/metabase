d695c70b1e747d7244c43f8fd3ba42d46283705b
Anton Kulyk <kuliks.anton@gmail.com>
2021-05-31 20:06:49 +0300

    Remove filtering logic from CollectionContent

--- frontend/src/metabase/collections/containers/CollectionContent.jsx
+++ frontend/src/metabase/collections/containers/CollectionContent.jsx
@@ -21,33 +21,12 @@ import { useListSelect } from "metabase/hooks/use-list-select";
 
 const PAGE_SIZE = 25;
 
-const MIN_ITEMS_TO_SHOW_FILTERS = 5;
-
-const ALL_MODELS = ["dashboard", "card", "snippet", "pulse"];
-
-const getModelsByFilter = filter => {
-  if (!filter) {
-    return ALL_MODELS;
-  }
-
-  return [filter];
-};
-
 const itemKeyFn = item => `${item.id}:${item.model}`;
 
-function CollectionContent({
-  collection,
-  collectionId,
-
-  isAdmin,
-  isRoot,
-  location,
-  router,
-}) {
+function CollectionContent({ collection, collectionId, isAdmin, isRoot }) {
   const [selectedItems, setSelectedItems] = useState(null);
   const [selectedAction, setSelectedAction] = useState(null);
-  const { handleNextPage, handlePreviousPage, setPage, page } = usePagination();
-  const [filter, setFilter] = useState(location.query.type || null);
+  const { handleNextPage, handlePreviousPage, page } = usePagination();
   const {
     selected,
     toggleItem,
@@ -98,22 +77,8 @@ function CollectionContent({
     setSelectedAction("copy");
   };
 
-  const handleFilterChange = useCallback(
-    type => {
-      router.push({
-        pathname: location.pathname,
-        search: type ? "?" + new URLSearchParams({ type }).toString() : null,
-      });
-
-      setFilter(type);
-      setPage(0);
-    },
-    [location.pathname, router, setPage],
-  );
-
   const unpinnedQuery = {
     collection: collectionId,
-    models: getModelsByFilter(filter),
     limit: PAGE_SIZE,
     offset: PAGE_SIZE * page,
     pinned_state: "is_not_pinned",
@@ -155,8 +120,6 @@ function CollectionContent({
               <Search.ListLoader query={unpinnedQuery} wrapped>
                 {({ list: unpinnedItems, metadata }) => {
                   const hasPagination = metadata.total > PAGE_SIZE;
-                  const showFilters =
-                    filter || unpinnedItems.length >= MIN_ITEMS_TO_SHOW_FILTERS;
 
                   const unselected = unpinnedItems.filter(
                     item => !getIsSelected(item),
@@ -183,16 +146,13 @@ function CollectionContent({
                   return (
                     <Box mt={hasPinnedItems ? 3 : 0}>
                       <ItemsTable
-                        filter={filter}
                         items={unpinnedItems}
                         empty={unpinnedItems.length === 0}
-                        showFilters={showFilters}
                         selectedItems={selected}
                         getIsSelected={getIsSelected}
                         collection={collection}
                         onToggleSelected={toggleItem}
                         onDrop={clear}
-                        onFilterChange={handleFilterChange}
                         onMove={handleMove}
                         onCopy={handleCopy}
                       />
